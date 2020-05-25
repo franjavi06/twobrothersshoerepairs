b9e2463f89c21eadf59e3f25159a8b26acb77949
Fran J <franjavi06@gmail.com>
2020-05-25 18:49:35 -0400

    fixed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,14 +5,20 @@ import Services from '../components/Services'
 import FeaturedProducts from '../components/FeaturedProducts';
 import GetNotified from '../components/GetNotified';
 
+
+
 class Home extends React.Component {
     constructor(props){
       super(props);
     }
     componentDidMount(){
+
       AOS.init({
         duration : 2000
       })
+
+    
+
     }
     render(){
       return(
